464ce33b6bbe60359f9739580d3751bce389360b
evansorrell2 <119913298+evansorrell2@users.noreply.github.com>
2023-12-06 22:40:50 -0500

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Awake() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(transform.position.magnitude > 15.0f) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force) {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        EnemyController e = other.gameObject.GetComponent<EnemyController>();
+        if (e != null) {
+            e.Fix();
+        }
+        Destroy(gameObject);
+    }
+}
