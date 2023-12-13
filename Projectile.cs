dde81a8df1f4d039840f3e005ae894fc875dc3ae
evansorrell2 <119913298+evansorrell2@users.noreply.github.com>
2023-12-13 18:42:33 -0500

    Add files via upload

--- Projectile.cs
+++ Projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
+    private Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Awake() {
--- RubyController.cs
+++ RubyController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-﻿public class RubyController : MonoBehaviour {
+public class RubyController : MonoBehaviour
+{
     public float speed = 3.0f;
     
     public int maxHealth = 5;
     
     public GameObject projectilePrefab;
+    
+    public AudioClip throwSound;
+    public AudioClip hitSound;
     
     public int health { get { return currentHealth; }}
     int currentHealth;
@@ -23,22 +27,29 @@ using UnityEngine;
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
     
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         
         Vector2 move = new Vector2(horizontal, vertical);
         
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
@@ -47,14 +58,29 @@ using UnityEngine;
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
         
-        if (isInvincible) {
+        if (isInvincible)
+        {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
         
-        if(Input.GetKeyDown(KeyCode.C)) {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
             Launch();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
         }
     }
     
@@ -67,13 +93,17 @@ using UnityEngine;
         rigidbody2d.MovePosition(position);
     }
 
-    public void ChangeHealth(int amount) {
-        if (amount < 0) {
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
             if (isInvincible)
                 return;
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            animator.SetTrigger("Hit");
+            PlaySound(hitSound);
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
@@ -81,12 +111,20 @@ using UnityEngine;
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     
-    void Launch() {
+    void Launch()
+    {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        
+        PlaySound(throwSound);
+    } 
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
